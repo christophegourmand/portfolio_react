ca0eff0cf4298db7ae96a48dd76c31800c9c26af
christophegourmand <christophe.gourmand@gmail.com>
2022-03-27 22:12:57 +0200

    add routes projects and contact

--- server.js
+++ server.js
@@ -3,10 +3,27 @@ const app = express();
 // NOTE: port set on 5000 instead of 3000 to avoid conflict with react who uses 3000.
 const PORT = 5000; 
 
+// --- set index|home page
 app.get(
     '/',
     (req,res) => {
-        res.send('<h1>Sent from server.js</h1>');
+        res.send('<h1>Homepage</h1>');
+    }
+);
+
+// --- set projects page
+app.get(
+    '/projects',
+    (req,res) => {
+        res.send('<h1>Projects</h1>');
+    }
+);
+
+// --- set contact page
+app.get(
+    '/contact',
+    (req,res) => {
+        res.send('<h1>Contact</h1>');
     }
 );
 
