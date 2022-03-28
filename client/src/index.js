47ae1c3a80e4d2a609affce0421c4088c2e47d27
christophegourmand <christophe.gourmand@gmail.com>
2022-03-28 23:35:09 +0200

    WIP: link react and express

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// import '........css'; // TODO: create CSS and activate line
+// import App from './App'; // TODO: activate line when file is written
+
+// --- NOTE : render <what> , <where>
+
+// temporary line
+    ReactDOM.render('<h1>MY APP</h1>' , document.getElementById('root'));
+
+// real line
+    // ReactDOM.render(<App /> , document.getElementById('root'));
--- server.js
+++ server.js
@@ -1,22 +1,41 @@
+// --- constants and imports
+
 const express = require('express');
+
 const app = express();
 // NOTE: port set on 5000 instead of 3000 to avoid conflict with react who uses 3000.
 const PORT = 5000;
 
+const path = require('path');
 
+
+// --- router  for projects
 const projectRouter = require('./routes/projects');
 app.use('/projects', projectRouter);
 
+// --- make public folder accessible for client
+// app.use('/public', express.static('public'));
+// -- or
+app.use('/public', express.static('client/public'));
 
 
 // --- set index|home page
-app.get(
-    '/',
-    (req,res) => {
-        res.send('<h1>Homepage</h1>');
-    }
-);
+    // before react was active
+        /*app.get(
+            '/',
+            (req,res) => {
+                res.send('<h1>Homepage</h1>');
+            }
+        );*/
 
+    // since react is active
+        app.get(
+            '/',
+            (req,res) => {
+                res.sendFile(path.join(__dirname, 'client','public','index.html'));
+            }
+        );
+        // LINK: https://levelup.gitconnected.com/how-to-render-react-app-using-express-server-in-node-js-a428ec4dfe2b
 
 
 // --- set contact page  
